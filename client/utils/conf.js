efa85047da2fce326a1bdb70f14dc9fec0b98a56
findstr <findstr@sina.com>
2019-01-01 15:16:11 +0800

    refine color style

--- client/utils/conf.js
+++ client/utils/conf.js
@@ -2,13 +2,13 @@ module.exports = {
        	server: 'https://cardhelper.weixin.gotocoding.com',
 	//server: 'http://192.168.2.118:9001',
 	banks: [
-		{ full: '中国银行', short: '中行', color: "orange"},
-		{ full: '建设银行', short: '建行', color: "blue"},
-		{ full: '工商银行', short: '工行', color: "purple"},
-		{ full: '交通银行', short: '交行', color: "mauve"},
-		{ full: '招商银行', short: '招行', color: "pink"},
-		{ full: '浦发银行', short: '浦发', color: "brown"},
-		{ full: '广发银行', short: '广发', color: "red"}
+		{ full: '中国银行', short: '中行', color: "white"},
+		{ full: '建设银行', short: '建行', color: "white"},
+		{ full: '工商银行', short: '工行', color: "white"},
+		{ full: '交通银行', short: '交行', color: "white"},
+		{ full: '招商银行', short: '招行', color: "white"},
+		{ full: '浦发银行', short: '浦发', color: "white"},
+		{ full: '广发银行', short: '广发', color: "white"}
 	],
 	TYPE_BILL: 1,
 	TYPE_REPAY: 2,
